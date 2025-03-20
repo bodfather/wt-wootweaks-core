1321d6f2c22f2e53df2411745bc2986fb3ce6321
bodfather <bodingtons@gmail.com>
2025-03-20 13:47:49 +0200

    v0.0.3 changed slug from wt_wootweaks to wt-wootweaks

--- wt-wootweaks-core.php
+++ wt-wootweaks-core.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: WooTweaks Core
 Description: The core menu for WooTweaks suite of plugins, enhancing WooCommerce functionality.
-Version: 0.0.2
+Version: 0.0.3
 Author: Bodfather
 License: GPLv2 or later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
